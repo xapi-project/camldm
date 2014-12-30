8200e5150e0edef5f57395e37e712fb2dc612597
David Scott <dave.scott@citrix.com>
2014-12-30 16:38:02 +0000

    Rename ocamlfind package to Devmapper (from Camldm)
    The rationale is
    - we already know we're using ocaml, no need to say 'caml'
    - the C interface we're binding to is called libdevmapper.h,
      so "devmapper" seems like a good name.
    
    Signed-off-by: David Scott <dave.scott@citrix.com>

--- lib_test/testcamldm.ml
+++ lib_test/testcamldm.ml
@@ -11,7 +11,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *)
-open Camldm
+open Devmapper
 open Utils
 
 let with_temp_file f =
