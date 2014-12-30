a5d2dcaf7222c87fd3ffc95930eb5c8e5f3654e1
David Scott <dave.scott@citrix.com>
2014-12-30 16:41:52 +0000

    travis: another attempt to fix
    Signed-off-by: David Scott <dave.scott@citrix.com>

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -26,7 +26,7 @@ opam --git-version
 echo Installing opam
 opam init -y
 eval `opam config env`
-opam pin add test-camldm . -n
+opam pin add test-camldm . -y
 sudo apt-get install $(opam install test-camldm -e ubuntu) -y
 opam install test-camldm -y
 make
