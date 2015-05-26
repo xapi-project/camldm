662187b453b6b44c2442fd03cbd6f45a70cdb579
David Scott <dave.scott@citrix.com>
2015-05-26 11:25:59 +0000

    Remove unnecessary libdevmapper.h dependency
    Signed-off-by: David Scott <dave.scott@citrix.com>

--- lib/camldm_stubs.c
+++ lib/camldm_stubs.c
@@ -11,7 +11,6 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  */
-#include <libdevmapper.h>
 #include <caml/mlvalues.h>
 #include <caml/memory.h>
 #include <caml/alloc.h>
