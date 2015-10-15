107c673ed758036af7cc7a01d5b33bf5fe926a25
Si Beaumont <simon.beaumont@citrix.com>
2015-10-15 11:37:33 +0100

    Restructure project
    Now it is compatible with opam and standard packaging. We have still avoided
    commiting the oasis autogen files. But now we have gone with:
    
    * A straight-forward ./configure script;
    * Having the tests we want in the _oasis file;
    * Having tests not unilaterally enabled;
    * Using an autogeneated (simpler) Makefile;
    * An opam file that includes fixes for the errors from `opam lint` (below).
    
    OPAM lint errors:
    
        $ opam lint
        Errors found in /local/work/code/camldm/opam:
                    error 25: Missing field 'authors'
                    error 33: Field 'os' is deprecated, use 'available' and the 'os'
                      variable instead
                    error 35: Missing field 'homepage'
                  warning 36: Missing field 'bug-reports'
                  warning 37: Missing field 'dev-repo'
    
    Authors filled in by using what `git` provides:
    
        $ git shortlog -sn
            72  David Scott
            20  Si Beaumont
             9  Dave Scott
             9  Jon Ludlam
             3  John Else
             2  Kaifeng Zhu
    
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- lib_test/testmock.ml
+++ lib_test/testmock.ml
@@ -123,40 +123,6 @@ let test_real_mock () =
   let module DM = (val dm : Devmapper.S.DEVMAPPER) in
   ignore (DM.ls ())
 
-let test_thread_safe () =
-  let pid = Unix.getpid () in
-  let exns = Hashtbl.create 10 in
-  let exns_m = Mutex.create () in
-  let th tid =
-    try
-      let names = Array.(make 10 ()
-      |> mapi (fun i _ -> Printf.sprintf "%d-%d-%d" pid tid i) |> to_list) in
-      List.iter (fun name ->
-        Mock.create name []; print_endline name) names;
-      List.iter (fun name ->
-        assert_bool (name ^ " not in Mock.ls ()") (List.mem name (Mock.ls ()))
-      ) names;
-      Thread.delay 0.1;
-      List.iter (fun name -> Mock.remove name) names;
-      List.iter (fun name ->
-        assert_bool (name ^ " in Mock.ls ()") (not (List.mem name (Mock.ls ())));
-      ) names
-    with exn ->
-      Mutex.lock exns_m;
-      Hashtbl.add exns tid exn;
-      Mutex.unlock exns_m in
-  let thread_ids = Array.(make 2 () |> mapi (fun i _ -> i) |> to_list) in
-  let threads = List.map (fun tid -> Thread.create th tid) thread_ids in
-  List.iter (fun t -> Thread.join t) threads;
-  if Hashtbl.length exns <> 0 then begin
-    Printf.printf "Some of the threads failed:\n";
-    Hashtbl.iter (fun k v ->
-      Printf.printf "\tProcess %d, thread %d failed with %s\n"
-        pid k (Printexc.to_string v)
-    ) exns;
-    exit 1
-  end
-
 let test_separate_paths () =
   Mock.set_path "dm-mock-1";
   Mock.clear ();
@@ -177,6 +143,5 @@ let suite = "devmapper_mock" >:::
     "test_reload" >:: test_reload;
     "test_create_remove" >:: test_create_remove;
     "test_real_mock" >:: test_real_mock;
-    "test_thread_safe" >:: test_thread_safe;
     "test_separate_paths" >:: test_separate_paths;
   ]
